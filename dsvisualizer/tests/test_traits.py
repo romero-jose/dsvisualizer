d94ddad19f4de4546e1ef6aeb192a9100f007d04
José Luis Romero <jose.romero@ing.uchile.cl>
2022-07-12 13:12:30 -0400

    Adds test for serialization of custom classes

--- dsvisualizer/tests/test_traits.py
+++ dsvisualizer/tests/test_traits.py
@@ -23,15 +23,45 @@ from dsvisualizer.traits import (
 def test_serialization_and_deserialization():
     operations = Operations(
         operations=[
-            Operation(Init(0, 10, None), Metadata(animate=False, source="n1 = Node(10)")),
-            Operation(Init(1, 11, 0), Metadata(animate=False, source="n2 = Node(11, n1)")),
-            Operation(Init(2, 12, 1), Metadata(animate=False, source="n3 = Node(12, n2)")),
+            Operation(
+                Init(0, 10, None), Metadata(animate=False, source="n1 = Node(10)")
+            ),
+            Operation(
+                Init(1, 11, 0), Metadata(animate=False, source="n2 = Node(11, n1)")
+            ),
+            Operation(
+                Init(2, 12, 1), Metadata(animate=False, source="n3 = Node(12, n2)")
+            ),
             Operation(GetValue(2), Metadata(animate=True, source="v = n3.value")),
             Operation(SetValue(2, 13), Metadata(animate=True, source="n3.value = 13")),
             Operation(GetNext(2), Metadata(animate=True, source="next = n3.next")),
             Operation(SetNext(2, 1), Metadata(animate=False, source="n3.next = n2")),
         ]
     )
+    serialized = serialize_operations(operations)
+    deserialized = deserialize_operations(serialized)
+    assert operations == deserialized
+    assert serialized == serialize_operations(deserialized)
+
+
+def test_serialization_of_custom_classes():
+    class MyCustomClass:
+        def __init__(self):
+            self.field = "test_value"
+
+        def __str__(self):
+            return f"MyCustomClass({self.field})"
+
+    c1 = MyCustomClass()
+    c2 = MyCustomClass()
+
+    operations = Operations(
+        operations=[
+            Operation(Init(0, str(c1), None), Metadata(animate=True, source="")),
+            Operation(SetValue(0, str(c2)), Metadata(animate=True, source="")),
+        ]
+    )
+
     serialized = serialize_operations(operations)
     deserialized = deserialize_operations(serialized)
     assert operations == deserialized
